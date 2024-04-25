c0a064aea893b656f15b0ceb75cef6a4c5747d55
Slymee <jetli_rijan@hotmail.com>
2024-04-25 19:07:19 +0545

    Admin authentication: wip

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->merge(['role' => auth()->check() && auth()->user()->is_super_admin ? 'admin' : 'user']);
+
+        return $next($request);
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin-dashboard', function () {
-    return view('superadmin.admin-dashboard');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin-dashboard', function () {
+        return view('superadmin.admin-dashboard');
+    });
 });
